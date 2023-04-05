da9d5b1ae00cd0138ac7ba3103a88f6de626824c
MINTEDCLICKS <121359923+MINTEDCLICKS@users.noreply.github.com>
2023-04-05 17:19:57 +0000

    Create Main.php

---
+++ src/MINTEDCLICKS/HealthOnKill/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MINTEDCLICKS\HealthOnKill;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable() : void{
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function onPlayerDeath(EntityDamageByEntityEvent $event) : void{
+$playerKilled = $event->getEntity();
+$killer = $event->getDamager();
+
+if($playerKilled instanceof Player && $killer instanceof Player){
+if($playerKilled->getHealth() - $event->getFinalDamage() <= 0){
+$killer->setHealth(20);
+}
+}
+}
+}
