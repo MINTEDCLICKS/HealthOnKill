d3fe860bb844731fbcbb076f8b9e563ca008aef7
MINTEDCLICKS <121359923+MINTEDCLICKS@users.noreply.github.com>
2023-04-06 23:53:15 +0000

    v1.1.0
    Made better overall

--- src/MINTEDCLICKS/HealthOnKill/Main.php
+++ src/MINTEDCLICKS/HealthOnKill/Main.php
@@ -6,6 +6,7 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerDeathEvent;
 
 class Main extends PluginBase implements Listener{
 
@@ -13,14 +14,11 @@ public function onEnable() : void{
 $this->getServer()->getPluginManager()->registerEvents($this, $this);
 }
 
-public function onPlayerDeath(EntityDamageByEntityEvent $event) : void{
-$playerKilled = $event->getEntity();
-$killer = $event->getDamager();
-
-if($playerKilled instanceof Player && $killer instanceof Player){
-if($playerKilled->getHealth() - $event->getFinalDamage() <= 0){
-$killer->setHealth($killer->getMaxHealth());
-}
+public function onPlayerDeath(PlayerDeathEvent $event) : void{
+$cause = $event->getPlayer()->getLastDamageCause(); //finds the cause of death
+if ($cause instanceof EntityDamageByEntityEvent){ //ensures that the following is only ran if the cause is due to a player killing
+$killer = $event->getPlayer()->getLastDamageCause()?->getDamager(); //finds the player who dealt the kill to the player killed
+$killer->setHealth($killer->getMaxHealth()); //sets health to max health. change getMaxHealth to getHealth()+NUMBER to add health instead of setting it to the max
 }
 }
 }
