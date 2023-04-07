04878aa5a472a239f39859230a7ef790e4c115ed
MINTEDCLICKS <121359923+MINTEDCLICKS@users.noreply.github.com>
2023-04-07 01:22:52 +0000

    1.1.1
    added a delay before health is changed to ensure compatibility with plugins that fetch health upon kill

--- src/MINTEDCLICKS/HealthOnKill/Main.php
+++ src/MINTEDCLICKS/HealthOnKill/Main.php
@@ -7,6 +7,11 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\scheduler\TaskHandler;
+use pocketmine\scheduler\Scheduler;
+use pocketmine\scheduler\Task;
+use pocketmine\scheduler\ClosureTask;
+
 
 class Main extends PluginBase implements Listener{
 
@@ -15,10 +20,12 @@ $this->getServer()->getPluginManager()->registerEvents($this, $this);
 }
 
 public function onPlayerDeath(PlayerDeathEvent $event) : void{
-$cause = $event->getPlayer()->getLastDamageCause(); //finds the cause of death
+$cause = $event->getPlayer()->getLastDamageCause(); //finds the case of death
 if ($cause instanceof EntityDamageByEntityEvent){ //ensures that the following is only ran if the cause is due to a player killing
 $killer = $event->getPlayer()->getLastDamageCause()?->getDamager(); //finds the player who dealt the kill to the player killed
-$killer->setHealth($killer->getMaxHealth()); //sets health to max health. change getMaxHealth to getHealth()+NUMBER to add health instead of setting it to the max
+$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use($killer) : void{ // used to delay the task (for compatibility with plugins that fetch player health upon a kill)
+$killer->setHealth($killer->getMaxHealth()); //sets the health max health after 1 tick (0.05 seconds) change getMaxHealth to getHealth()+NUMBER to add health instead of setting it to the max
+}), 1); 
 }
 }
 }
